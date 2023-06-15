cb4e9f12d445c2cb806040cdc367a2f84827fad8
Filipe Dervelan <filipeedervelan@gmail.com>
2023-06-15 12:21:24 -0300

    feat: adding post titles to the page

--- my-blog/src/pages/Dashboard/Dashboard.jsx
+++ my-blog/src/pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,34 @@
 import styles from './Dashboard.module.css';
 
+import { Link } from 'react-router-dom';
+
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 export const Dashboard = () => {
+	const { user } = useAuthValue();
+	const uid = user.uid;
+
+	const { documents: posts, loading } = useFetchDocuments('posts', null, uid);
+
 	return (
 		<div>
 			<h1>dashshhh</h1>
+			<p>manage your posts</p>
+			{posts && posts.length === 0 ? (
+				<div className={styles.noposts}>
+					<p>Posts not found</p>
+					<Link to="/posts/create" className="btn">
+						Create first post
+					</Link>
+				</div>
+			) : (
+				<div>
+					<p>postsss</p>
+				</div>
+			)}
+
+			{posts && posts.map((post) => <h3 key={post.id}>{post.title}</h3>)}
 		</div>
 	);
 };
