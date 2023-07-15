389382fde69fe457d6665d814fd7582e9d512a02
Filipe Dervelan <filipeedervelan@gmail.com>
2023-07-15 16:01:53 -0300

    feat: photo router

--- reactgram-backend/routes/Router.js
+++ reactgram-backend/routes/Router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express();
 
 router.use('/api/users', require('./UserRoutes'));
+router.use('/api/photos', require('./PhotoRoutes'));
 
 router.get('/hello-world', (req, res) => {
 	res.send('Hello! World!');
