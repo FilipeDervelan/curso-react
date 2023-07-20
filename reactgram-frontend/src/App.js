87714955c43c9f6cdfed40837e3ae169b2e0ee20
Filipe Dervelan <filipeedervelan@gmail.com>
2023-07-19 22:55:11 -0300

    feat: new routes and checks if user is logged

--- reactgram-frontend/src/App.js
+++ reactgram-frontend/src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
 import './App.css';
 
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+// Components
+
+// Hooks
+import { useAuth } from './Hooks/useAuth';
+
+// Pages
+import { Home } from './pages/Home/Home';
+import { Login } from './pages/Auth/Login';
+import { Register } from './pages/Auth/Register';
+import { Navbar } from './components/Navbar';
+import { Footer } from './components/Footer';
+import { EditProfile } from './pages/EditProfile/EditProfile';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { auth, loading } = useAuth();
+
+	if (loading) {
+		return <p>Carregando...</p>;
+	}
+
+	return (
+		<div className="App">
+			<BrowserRouter>
+				<Navbar />
+				<div className="container">
+					<Routes>
+						<Route
+							path="/"
+							element={auth ? <Home /> : <Navigate to="/login" />}
+						/>
+						<Route
+							path="/profile"
+							element={auth ? <EditProfile /> : <Navigate to="/login" />}
+						/>
+						<Route
+							path="/login"
+							element={!auth ? <Login /> : <Navigate to="/" />}
+						/>
+						<Route
+							path="/register"
+							element={!auth ? <Register /> : <Navigate to="/" />}
+						/>
+					</Routes>
+				</div>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
